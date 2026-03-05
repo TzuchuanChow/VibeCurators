f5cadd841ec40a6e83eb17f4621a133e8654f4d5
Kevin <145588268+xKevBac@users.noreply.github.com>
2026-03-05 08:31:27 -0800

    added Movies.jsx and Install Router package

--- VibeLensWebPage/VibeLens/src/main.jsx
+++ VibeLensWebPage/VibeLens/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './index.css'
 import App from './App.jsx'
+import Movies from './Movies.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* ... other routes */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
